301663cf08582ce5d0762f8dce44ee6d35def53f
Bruce Becker <brucellino@protonmail.ch>
2024-08-03 11:29:40 +0200

    chore: add initial dependencies for Mongo
    Signed-off-by: Bruce Becker <brucellino@protonmail.ch>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # from typing import Union
 
 from fastapi import FastAPI
+from robot import run
 
 app = FastAPI()
 
@@ -8,3 +9,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/test", status_code=201)
+async def run_test():
+    run("self.robot")
---
+++ robot/self.robot
@@ -0,0 +1,7 @@
+*** Settings ***
+Library     OperatingSystem
+
+
+*** Test Cases ***
+I am Ok
+    File Should Exist    self.robot
